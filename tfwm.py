3f500d2dff8325bf53f92feb99896823f7b1e638
KRoperUK <13541920+KRoperUK@users.noreply.github.com>
2024-02-08 00:47:54 +0000

    Add metro stops and arrivals to map

---
+++ tfwm.py
@@ -0,0 +1,61 @@
+import requests
+import urllib.parse
+import os
+import xml.etree.ElementTree as ET 
+from dotenv import load_dotenv
+import xml.dom.minidom
+from siri import parse
+import yaml
+import json
+from copy import deepcopy
+
+load_dotenv()
+
+MM_LINE_ID = "220"
+
+PARAMS = {
+    'app_key': os.getenv('app_key'),
+    'app_id': os.getenv('app_id'),
+    'formatter': 'json'
+}
+
+# def get_stops():
+#     URL = f"http://api.tfwm.org.uk/Line/{MM_LINE_ID}/StopPoints?"
+#     resp = requests.get(URL, params=PARAMS)
+#     return resp.json()
+
+# # raw_metro_stops = get_stops()
+
+# # with open('metro_stops.json', 'w') as f:
+# #     f.write(json.dumps(raw_metro_stops))
+
+# with open('metro_stops.json', 'r') as f:
+#     raw_metro_stops = json.load(f)
+
+# metro_stops = []
+
+# for stop in raw_metro_stops['ArrayOfStopPoint']['StopPoint']:
+#     metro_stops.append(stop)
+
+# # Dump metro_stops array into YAML file
+# with open('metro_stops.yaml', 'w') as f:
+#     yaml.dump(metro_stops, f)
+
+with open('metro_stops.yaml', 'r') as f:
+    metro_stops = yaml.load(f, Loader=yaml.FullLoader)
+
+def getArrivals():
+    URL = f"http://api.tfwm.org.uk/Line/{MM_LINE_ID}/Arrivals?"
+    resp = requests.get(URL, params=PARAMS)
+    return resp.json()
+
+raw_metro_arrivals = getArrivals()
+
+metro_arrivals = []
+
+for arrival in raw_metro_arrivals['ArrayOfPrediction']['Prediction']:
+    metro_arrivals.append(arrival)
+
+with open('metro_arrivals.yaml', 'w') as f:
+    yaml.dump(metro_arrivals, f)
+
